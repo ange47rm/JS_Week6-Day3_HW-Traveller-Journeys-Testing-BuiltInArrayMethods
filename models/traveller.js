9b5854ecb75c2542aa4b9b30a1530255adf6de67
ange47rm <ange47rm@hotmail.com>
2021-01-07 18:05:20 +0000

    Passed 8 tests

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,21 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map ( function (journey){
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map ( (journey) => {   // USING ARROW FUNCTION HERE
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter ( (journey) => {
+    return transport === journey.transport;
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
